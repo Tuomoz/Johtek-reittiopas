252b7165ef42cad8a33ab0b4aa61f1b35a48f5f7
Tuomo Kärkkäinen <tuomon.email@gmail.com>
2016-01-28 13:46:22 +0200

    BFS-haku tehty loppuun

--- src/main/java/fi/helsinki/cs/reittiopas/Reittiopas.java
+++ src/main/java/fi/helsinki/cs/reittiopas/Reittiopas.java
@@ -1,6 +1,7 @@
 package fi.helsinki.cs.reittiopas;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -21,18 +22,28 @@ public class Reittiopas {
      * @return Tila-olioista koostuva linkitetty lista maalista lähtötilaan
      */
     public Tila haku(Pysakki lahto, Pysakki maali) {
-        lahto.getNaapurit();
-
-        Tila edellinenTila = new Tila(lahto, null);
-        LinkedList solmulista = new LinkedList<Pysakki>();
-        HashSet kasitellyt = new HashSet<Pysakki>();
+        LinkedList<Pysakki> solmulista = new LinkedList<>();
+        HashSet<Pysakki> kasitellyt = new HashSet<>();
+        HashMap<Pysakki, Tila> pysakkienTilat = new HashMap<>();
+        
+        Tila lahtotila = new Tila(lahto, null);
+        pysakkienTilat.put(lahto, lahtotila);
+        solmulista.add(lahto);
+        
         while (!solmulista.isEmpty()) {
             Pysakki solmu = (Pysakki) solmulista.pop();
-            if (!kasitellyt.contains(solmu)) {
-                kasitellyt.add(solmu);
-                solmulista.addAll(solmu.getNaapurit());
+            Tila solmunTila = pysakkienTilat.get(solmu);
+            if (solmu.equals(maali))
+                return solmunTila;
+            
+            for (Pysakki naapuri : solmu.getNaapurit()) {
+                if (!kasitellyt.contains(naapuri)) {
+                    kasitellyt.add(naapuri);
+                    solmulista.add(naapuri);
+                    pysakkienTilat.put(naapuri, new Tila(naapuri, solmunTila));
+                }
             }
         }
-        return edellinenTila;
+        return null;
     }
 }
